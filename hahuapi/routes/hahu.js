7304c5d4040bff6fe3966c22cb679b7cf17b63f5
Meronka <91194030+Meronka@users.noreply.github.com>
2022-03-23 10:15:42 +0100

    1000-rel való oszthatóság ellenőrzése

---
+++ hahuapi/routes/hahu.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+var Hirdetes = require('../models/hirdetes');
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    const _id = req.body._id;
+    const kategoria = req.body.kategoria;
+    const cim = req.body.cim;
+    const leiras = req.body.leiras;
+    const hirdetesDatuma = req.body.hirdetesDatuma;
+    const serulesmentes = req.body.serulesmentes;
+    const arFt = req.body.arFt;
+    const kepUrl = req.body.kepUrl;
+
+    try {
+        if(arFt % 1000 != 0){
+            throw Error("Az ár nem osztható ezerrel")
+        }
+        const hirdetes = new Hirdetes({_id, 
+            kategoria, 
+            cim, 
+            leiras, 
+            hirdetesDatuma, 
+            serulesmentes, 
+            arFt, 
+            kepUrl})
+        hirdetes
+        .save()
+        .then(res.json({
+            "message": "A rekord rögzítése sikeres!"
+        }))
+    .catch(err => {
+            console.log(err)
+        })
+    } catch (err) {
+        res.status(400).json({
+            "error": err.message
+        })
+    }
+
+    
+});
+
+module.exports = router;
